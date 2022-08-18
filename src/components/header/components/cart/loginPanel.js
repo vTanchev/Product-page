cf348e20334b4e9124bad3f847e89913da79ccf8
Vasko <vaskotanchev@yahoo.com>
2022-08-18 17:46:37 +0200

    header component is completed

--- src/components/header/components/cart/loginPanel.js
+++ src/components/header/components/cart/loginPanel.js
@@ -10,7 +10,11 @@ const LoginPanel = () => {
         alt="Avatar Logo"
         height={40}
         width={40}
-        style={{ border: "1px solid hsl(26, 100%, 55%)", borderRadius: "50%" }}
+        style={{
+          border: "1px solid hsl(26, 100%, 55%)",
+          borderRadius: "50%",
+          cursor: "pointer",
+        }}
       />
     </div>
   );
